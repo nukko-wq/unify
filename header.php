09cb16d57ad77fb37430bf690abb20f88a16f5a7
nukko <shouta.tsuboi@gmail.com>
2024-12-06 12:05:17 +0900

    不要な記述の削除

--- header.php
+++ header.php
@@ -19,7 +19,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/normalize.css">
 	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
 
 	<?php wp_head(); ?>
 </head>
